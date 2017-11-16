b659b58b1596f035811f97de2e1a8b471ada037a
Andrei Sambra <a.sambra@qwant.com>
2017-11-16 12:12:57 +0100

    updated API to support origin-bound stores

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
 ;(function (root) {
-  var CrossStorageHub = {}
+  var MasqHub = {}
+
+  MasqHub._log = function (text) {
+    if (MasqHub._debug) {
+      console.log(text)
+    }
+  }
 
   /**
-   * Accepts an array of objects with two keys: origin and allow. The value
-   * of origin is expected to be a RegExp, and allow, an array of strings.
-   * The cross storage hub is then initialized to accept requests from any of
-   * the matching origins, allowing access to the associated lists of methods.
-   * Methods may include any of: get, set, del, getKeys and clear. A 'ready'
-   * message is sent to the parent window once complete.
+   * Forked from https://github.com/zendesk/cross-storage
    *
+   * Accepts an array of objects used for configuration:
+   *  - an array of permissions containing objects with two keys: origin and allow.
+   *    The value of origin is expected to be a RegExp, and allow, an array of strings.
+   *    The cross storage hub is then initialized to accept requests from any of
+   *    the matching origins, allowing access to the associated lists of methods.
+   *    Methods may include any of: get, set, del, getData and clear. A 'ready'
+   *    message is sent to the parent window once complete.
+   *  - debug flag
    * @example
    * // Subdomain can get, but only root domain can set and del
-   * CrossStorageHub.init([
-   *   {origin: /\.example.com$/,        allow: ['get']},
-   *   {origin: /:(www\.)?example.com$/, allow: ['get', 'set', 'del']}
-   * ]);
+   * MasqHub.init({
+   *   permissions: [{origin: /\.example.com$/,        allow: ['get']},
+   *    {origin: /:(www\.)?example.com$/, allow: ['get', 'set', 'del']}],
+   *   debug: false
+   * });
    *
-   * @param {array} permissions An array of objects with origin and allow
+   * @param {array} parameters An array of objects used for configuration
    */
-  CrossStorageHub.init = function (permissions) {
+  MasqHub.init = function (parameters) {
     var available = true
 
     // Return if localStorage is unavailable, or third party
@@ -37,9 +47,12 @@
       }
     }
 
-    CrossStorageHub._permissions = permissions || []
-    CrossStorageHub._installListener()
+    MasqHub._debug = parameters.debug || false
+    MasqHub._permissions = parameters.permissions || []
+    MasqHub._installListener()
     window.parent.postMessage('cross-storage:ready', '*')
+
+    MasqHub._log('Listening to clients...')
   }
 
   /**
@@ -48,8 +61,8 @@
    *
    * @private
    */
-  CrossStorageHub._installListener = function () {
-    var listener = CrossStorageHub._listener
+  MasqHub._installListener = function () {
+    var listener = MasqHub._listener
     if (window.addEventListener) {
       window.addEventListener('message', listener, false)
     } else {
@@ -60,12 +73,12 @@
   /**
    * The message handler for all requests posted to the window. It ignores any
    * messages having an origin that does not match the originally supplied
-   * pattern. Given a JSON object with one of get, set, del or getKeys as the
+   * pattern. Given a JSON object with one of get, set, del or getData as the
    * method, the function performs the requested action and returns its result.
    *
    * @param {MessageEvent} message A message to be processed
    */
-  CrossStorageHub._listener = function (message) {
+  MasqHub._listener = function (message) {
     var origin, targetOrigin, request, method, error, result, response
 
     // postMessage returns the string "null" as the origin for "file://"
@@ -95,21 +108,23 @@
 
     if (!method) {
       return
-    } else if (!CrossStorageHub._permitted(origin, method)) {
+    } else if (!MasqHub._permitted(origin, method)) {
       error = 'Invalid permissions for ' + method
     } else {
       try {
-        result = CrossStorageHub['_' + method](request.params)
+        result = MasqHub['_' + method](origin, request.params)
       } catch (err) {
         error = err.message
       }
     }
 
     response = JSON.stringify({
-      id: request.id,
+      client: request.client,
       error: error,
       result: result
     })
+
+    MasqHub._log('Received data:' + response)
 
     // postMessage requires that the target origin be set to "*" for "file://"
     targetOrigin = (origin === 'file://') ? '*' : origin
@@ -120,28 +135,28 @@
   /**
    * Returns a boolean indicating whether or not the requested method is
    * permitted for the given origin. The argument passed to method is expected
-   * to be one of 'get', 'set', 'del' or 'getKeys'.
+   * to be one of 'get', 'set', 'del', 'clear', 'getData' or 'setData'.
    *
    * @param   {string} origin The origin for which to determine permissions
    * @param   {string} method Requested action
    * @returns {bool}   Whether or not the request is permitted
    */
-  CrossStorageHub._permitted = function (origin, method) {
+  MasqHub._permitted = function (origin, method) {
     var available, i, entry, match
 
-    available = ['get', 'set', 'del', 'clear', 'getKeys']
-    if (!CrossStorageHub._inArray(method, available)) {
+    available = ['get', 'set', 'del', 'clear', 'getData', 'setData']
+    if (!MasqHub._inArray(method, available)) {
       return false
     }
 
-    for (i = 0; i < CrossStorageHub._permissions.length; i++) {
-      entry = CrossStorageHub._permissions[i]
+    for (i = 0; i < MasqHub._permissions.length; i++) {
+      entry = MasqHub._permissions[i]
       if (!(entry.origin instanceof RegExp) || !(entry.allow instanceof Array)) {
         continue
       }
 
       match = entry.origin.test(origin)
-      if (match && CrossStorageHub._inArray(method, entry.allow)) {
+      if (match && MasqHub._inArray(method, entry.allow)) {
         return true
       }
     }
@@ -150,12 +165,16 @@
   }
 
   /**
-   * Sets a key to the specified value.
+   * Sets a key to the specified value, based on the origin of the request.
    *
+   * @param {string} origin The origin of the request
    * @param {object} params An object with key and value
    */
-  CrossStorageHub._set = function (params) {
-    window.localStorage.setItem(params.key, params.value)
+  MasqHub._set = function (origin, params) {
+    // TODO throttle writing to once per second
+    var data = MasqHub._getData(origin)
+    data[params.key] = params.value
+    window.localStorage.setItem(origin, JSON.stringify(data))
   }
 
   /**
@@ -163,18 +182,20 @@
    * Returns a single value if only one key was supplied, otherwise it returns
    * an array. Any keys not set result in a null element in the resulting array.
    *
+   * @param   {string} origin The origin of the request
    * @param   {object} params An object with an array of keys
    * @returns {*|*[]}  Either a single value, or an array
    */
-  CrossStorageHub._get = function (params) {
-    var storage, result, i, value
+  MasqHub._get = function (origin, params) {
+    var data, result, i, value
 
-    storage = window.localStorage
     result = []
+
+    data = MasqHub._getData(origin)
 
     for (i = 0; i < params.keys.length; i++) {
       try {
-        value = storage.getItem(params.keys[i])
+        value = data[params.keys[i]]
       } catch (e) {
         value = null
       }
@@ -188,37 +209,48 @@
   /**
    * Deletes all keys specified in the array found at params.keys.
    *
+   * @param {string} origin The origin of the request
    * @param {object} params An object with an array of keys
    */
-  CrossStorageHub._del = function (params) {
+  MasqHub._del = function (origin, params) {
+    var data = MasqHub._getData(origin)
     for (var i = 0; i < params.keys.length; i++) {
-      window.localStorage.removeItem(params.keys[i])
+      delete data[params.keys[i]]
     }
+    window.localStorage.setItem(JSON.stringify(data))
   }
 
   /**
    * Clears localStorage.
+   *
+   * @param {string} origin The origin of the request
    */
-  CrossStorageHub._clear = function () {
-    window.localStorage.clear()
+  MasqHub._clear = function (origin) {
+    window.localStorage.removeItem(origin)
   }
 
   /**
-   * Returns an array of all keys stored in localStorage.
+   * Returns all data limited to the scope of the origin.
    *
-   * @returns {string[]} The array of keys
+   * @param   {string} origin The origin of the request
+   * @returns {object} The data corresponding to the origin
    */
-  CrossStorageHub._getKeys = function (params) {
-    var i, length, keys
-
-    keys = []
-    length = window.localStorage.length
-
-    for (i = 0; i < length; i++) {
-      keys.push(window.localStorage.key(i))
+  MasqHub._getData = function (origin) {
+    var data = window.localStorage.getItem(origin)
+    if (!data || data.length === 0) {
+      return {}
     }
+    return JSON.parse(data)
+  }
 
-    return keys
+  /**
+   * Sets all data limited to the scope of the origin.
+   *
+   * @param   {string} origin The origin of the request
+   * @param   {object} data The data payload
+   */
+  MasqHub._setData = function (origin, data) {
+    window.localStorage.setItem(origin, data)
   }
 
   /**
@@ -230,7 +262,7 @@
    * @parma   {[]*}  array The array in which to search
    * @returns {bool} Whether or not the value was found
    */
-  CrossStorageHub._inArray = function (value, array) {
+  MasqHub._inArray = function (value, array) {
     for (var i = 0; i < array.length; i++) {
       if (value === array[i]) return true
     }
@@ -239,12 +271,22 @@
   }
 
   /**
+   * Returns whether or not an object is empty.
+   *
+   * @param   {object} obj The object to check
+   * @returns {bool} Whether or not the object is empoty
+   */
+  MasqHub._isEmpty = function (obj) {
+    return Object.keys(obj).length === 0
+  }
+
+  /**
    * A cross-browser version of Date.now compatible with IE8 that avoids
    * modifying the Date object.
    *
    * @return {int} The current timestamp in milliseconds
    */
-  CrossStorageHub._now = function () {
+  MasqHub._now = function () {
     if (typeof Date.now === 'function') {
       return Date.now()
     }
@@ -256,14 +298,14 @@
    * Export for various environments.
    */
   if (typeof module !== 'undefined' && module.exports) {
-    module.exports = CrossStorageHub
+    module.exports = MasqHub
   } else if (typeof exports !== 'undefined') {
-    exports.CrossStorageHub = CrossStorageHub
+    exports.MasqHub = MasqHub
   } else if (typeof define === 'function' && define.amd) {
     define([], function () {
-      return CrossStorageHub
+      return MasqHub
     })
   } else {
-    root.CrossStorageHub = CrossStorageHub
+    root.MasqHub = MasqHub
   }
 }(this))
